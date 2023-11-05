b5fe430e106c59bfeda92ffe8fc14bcba1ab24eb
Taner ÜZÜM <uzumtaner.official@gmail.com>
2023-11-06 01:49:22 +0300

    second commit - check

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv').config()
 connectDb()
 const app = express()
 const port = process.env.PORT || 5000;
-//Belirli bir port verilmediyse default olarak 5000 de çalışır.
 
 /** app.get('/api/contacts', (req, res) => {
     //res.send('Get all contactss.')
